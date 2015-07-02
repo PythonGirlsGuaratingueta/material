3b1f164ef6e926b831f7b948e4a167b17d91cac2
Jonatas Leon <jonatas.leon@gmail.com>
2015-07-02 13:45:26 -0300

    Exercicios 05 a 08 - Jonatas

---
+++ lista de exercicios/primeira_lista_exercicio/Jonatas/exerc06.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.4
+# -*- coding: utf-8 -*-
+
+r = float(input('Digite o valor do raio da circunferência(cm): '))
+pi = 3.14
+area = pi * r ** 2
+
+print('{area}cm² equivale a área da circunferência'.format(area=area))
