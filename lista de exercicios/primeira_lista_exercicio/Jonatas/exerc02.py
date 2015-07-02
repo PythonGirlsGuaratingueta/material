3f13859b1cf74d07cb1a6c64c1f627763df0a533
Jonatas Leon <jonatas.leon@gmail.com>
2015-07-02 01:09:28 -0300

    Exercicios 01 ao 04

---
+++ lista de exercicios/primeira_lista_exercicio/Jonatas/exerc02.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3.4
+# -*- coding: utf-8 -*-
+
+numero = input('Informe um número: ')
+print('O número informado foi {n}'.format(n=numero))
---
+++ lista de exercicios/primeira_lista_exercicio/Jonatas/exerc03.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3.4
+# -*- coding: utf-8 -*-
+
+n1 = int(input("Digite o 1º número: "))
+n2 = int(input("Digite o 2º número: "))
+
+print("{n1} + {n2} = {soma}".format(n1=n1, n2=n2, soma=(n1+n2)))
