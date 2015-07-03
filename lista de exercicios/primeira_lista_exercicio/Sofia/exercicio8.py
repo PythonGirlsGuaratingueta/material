66711b7573f3a2b4053411620ac185ba6ecdba5f
Sofyi <sofia.big@live.com>
2015-07-03 03:15:37 -0300

    Exercicio 8

---
+++ lista de exercicios/primeira_lista_exercicio/Sofia/exercicio8.py
@@ -0,0 +1,5 @@
+qutdHora =int(input("Quantas horas foram trabalhadas no mês: "))
+valorHora=float(input("Quanto vale a hora: "))
+salario=qutdHora*valorHora
+
+print("Salario no fim do mes: R$ {:.2f}".format(salario))
