478cd13020a7d81201071e8bacadc244222c072b
Sofyi <sofia.big@live.com>
2015-07-03 04:31:08 -0300

    Exercicio 12 ao 15 na mesma questao

---
+++ lista de exercicios/primeira_lista_exercicio/Sofia/exercicicio12_13_14_15.py
@@ -0,0 +1,10 @@
+num1=int(input("Insira um numero inteiro: "))
+num2=int(input("Insira outro numero inteiro: "))
+num3=float(input("Agora insira um numero real: "))
+
+conta1=(num1*2)*(num2/2)
+conta2=(num1*3)+num3
+conta3=num3**3
+print ("Conta 1: ",conta1)
+print ("Conta 2: ",conta2)
+print ("Conta 3: {:.2f}".format(conta3))
